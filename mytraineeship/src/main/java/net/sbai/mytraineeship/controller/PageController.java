2dab7cba6f89d443bad39fe2c9b99f78b72ef8a9
sbai zouhour <snewdeve@gmail.com>
2017-09-24 03:11:09 +0200

    4 5

--- mytraineeship/src/main/java/net/sbai/mytraineeship/controller/PageController.java
+++ mytraineeship/src/main/java/net/sbai/mytraineeship/controller/PageController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.sbai.onlinebackend.dao.CategoryDAO;
+import net.sbai.onlinebackend.dao.ProductDAO;
 import net.sbai.onlinebackend.dto.Category;
+import net.sbai.onlinebackend.dto.Product;
 
 @Controller
 public class PageController {
 	
 	@Autowired
 	private CategoryDAO categoryDAO;
+	
+	@Autowired
+	private ProductDAO productDAO;
 	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {		
@@ -80,6 +85,39 @@ public class PageController {
 		mv.addObject("userClickCategoryProducts",true);
 		return mv;				
 	}	
+	
+	
+	/*
+	 * Viewing a single product
+	 * */
+	
+	@RequestMapping(value = "/show/{id}/product") 
+	public ModelAndView showSingleProduct(@PathVariable int id) {
+		
+		ModelAndView mv = new ModelAndView("page");
+		
+		Product product = productDAO.get(id);
+		
+		// update the view count
+		product.setViews(product.getViews() + 1);
+		productDAO.update(product);
+		//---------------------------
+		
+		mv.addObject("title", product.getName());
+		mv.addObject("product", product);
+		
+		mv.addObject("userClickShowProduct", true);
+		
+		
+		return mv;
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
